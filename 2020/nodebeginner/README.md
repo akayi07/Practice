fb3ed5d71b9a739eb5ceb63a523d3a832308da33
akayi07 <zhuolimiaotx@163.com>
2020-06-09 17:27:41 +0800

    aa

---
+++ 2020/nodebeginner/README.md
@@ -0,0 +1,3 @@
+# front-end
+前端练习, 基础练习和各教程的项目练习<br>
+Just some basic exercises and practices of tutorials
---
+++ 2020/nodebeginner/index.js
@@ -0,0 +1,9 @@
+var server = require('./server-1');
+var router = require('./router');
+var requestHandlers = require('./requestHandlers');
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start; 
+handle["/upload"] = requestHandlers.upload;
+server.start(router.route, handle);
